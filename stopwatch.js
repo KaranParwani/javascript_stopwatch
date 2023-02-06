615c6d3ea7a6a69bef1d1e913b6054c092b19e17
Karan <karanparwani006@gmail.com>
2023-02-06 21:05:02 +0530

    added functionality for stopwatch

--- stopwatch.js
+++ stopwatch.js
@@ -3,27 +3,30 @@ let min = 0;
 let sec = 0;
 let milliseconds = 0;
 
-
+// timer variable to control the functionality like start, stop, reset
 let timer = 0;
 
+// function to start the stopwatch
 function start() {
     timer = 1;
     stopWatch()
 }
 
+// function to stop the time
 function stop() {
     timer = 2;
     stopWatch()
 }
 
+// function to reset the timer
 function reset() {
     timer = 3;
     stopWatch()
 }
 
-
+// main function
 function stopWatch() {  
-    
+
     if (timer == 1){
         milliseconds = milliseconds + 1;
         
@@ -50,10 +53,25 @@ function stopWatch() {
         setTimeout("stopWatch()", 10);
     }
 
-    else{
+    else if (timer == 3){
+        
         document.getElementById("seconds").innerHTML = 0;
         document.getElementById("minutes").innerHTML = 0;
         document.getElementById("hours").innerHTML = 0;
         document.getElementById("milliseconds").innerHTML = 0;
-     }
+        document.getElementById("lap").innerHTML = '';
+    }
+
+    else if (timer == 2){
+
+        document.getElementById("seconds").innerHTML = sec;
+        document.getElementById("minutes").innerHTML = min;
+        document.getElementById("hours").innerHTML = hr;
+        document.getElementById("milliseconds").innerHTML = milliseconds;
+
+        lapping = hr + ":" + min + ":" + sec + ":" + milliseconds;
+        document.getElementById("lap").innerHTML = lapping;
+
+    }
+
 }
